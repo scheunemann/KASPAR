b3267a78070cbb33fd28421f7b2c79057efc20a2
Nathan Burke <natbur@gmail.com>
2014-03-11 16:23:49 +0000

    handle optional actions on triggers

--- Web/api/modules/interaction.py
+++ Web/api/modules/interaction.py
@@ -12,6 +12,7 @@ from Model.log import InteractionLog
 
 interactionManagers = {}
 
+
 class UserAction(object):
     exposed = True
 
@@ -23,13 +24,13 @@ class UserAction(object):
             cherrypy.request.params['interactionId'] = vpath.pop(0)
         if not vpath[0].isdigit():
             return getattr(self, vpath.pop(0), None)
-        
+
     @cherrypy.tools.json_out()
     def GET(self, interactionId, logId=None, timestamp=None):
         if logId:
             logs = cherrypy.request.db.query(Model.InteractionLog).get(logId)
         else:
-            logs = cherrypy.request.db.query(Model.InteractionLog).filter(Model.InteractionLog.interaction_id==interactionId)
+            logs = cherrypy.request.db.query(Model.InteractionLog).filter(Model.InteractionLog.interaction_id == interactionId)
 
         ret = []
         for log in logs:
@@ -40,7 +41,7 @@ class UserAction(object):
                            'timestamp': Helper._utcDateTime(log.timestamp),
                            'active': log.finished == None,
                         })
-            
+
         return ret[0] if logId else ret
 
     @cherrypy.tools.json_in()
@@ -50,11 +51,11 @@ class UserAction(object):
             triggerId = cherrypy.request.json['trigger_id']
         else:
             raise cherrypy.HTTPError("Invalid JSON, missing 'trigger_id'")
-        
+
         if interactionId not in interactionManagers:
             raise cherrypy.HTTPError("Cannot update an inactive interaction.")
-        
-        exists = cherrypy.request.db.query(Data.Model.Trigger).filter(Data.Model.Trigger.id==triggerId).count() > 0
+
+        exists = cherrypy.request.db.query(Data.Model.Trigger).filter(Data.Model.Trigger.id == triggerId).count() > 0
         if not exists:
             raise cherrypy.NotFound("Invalid trigger id: %s" % triggerId)
 
@@ -74,10 +75,10 @@ class UserAction(object):
 class Interaction(ModelCRUD):
     exposed = True
     useraction = UserAction()
-    
+
     def __init__(self):
         super(Interaction, self).__init__(Model.model.Interaction, ['GET', 'POST'])
-        
+
     @cherrypy.tools.json_in()
     @cherrypy.tools.json_out()
     def POST(self, oid=None, **constraint):
@@ -87,13 +88,14 @@ class Interaction(ModelCRUD):
             interactionManagers[interaction.id] = InteractionManager(interaction)
             interactionManagers[interaction.id].start()
         elif interaction and interaction.endTime:
-            if interaction.id in interactionManagers: 
+            if interaction.id in interactionManagers:
                 interactionManagers[interaction.id].stop()
-            
+
         return interaction.serialize(urlResolver=self._urlResolver, resolveProps=resolveList)
 
+
 class InteractionManager(object):
-        
+
     def __init__(self, interaction):
         self._interactionId = interaction.id
         triggers = self._getTriggers(interaction.robot, interaction.user)
@@ -102,23 +104,23 @@ class InteractionManager(object):
         self._actionRunner = ActionRunner(interaction.robot)
         self._handles = {}
         self._handleLock = RLock()
-        
+
     def start(self):
         self._triggerProcessor.start()
-    
+
     def stop(self):
         self._triggerProcessor.stop()
-        
+
     @property
     def activeActions(self):
         with self._handleLock:
             return self._handles.keys()
-        
+
     def stopAction(self, actionId):
         with self._handleLock:
             if actionId in self._handles:
                 self._handles[actionId].stop()
-        
+
     def _handleComplete(self, handle, logId=None):
         ds = StorageFactory.getNewSession()
         if logId:
@@ -144,9 +146,10 @@ class InteractionManager(object):
         log.trigger_value = value
         ds.add(log)
         ds.commit()
-        action = ds.query(Data.Model.Action).join(Data.Model.Trigger).filter(Data.Model.Trigger.id==trigger_id).first()
-        with self._handleLock:
-            if action.id in self._handles:
-                self._handles[action.id].stop()
-            self._handles[action.id] = self._actionRunner.executeAsync(action, self._handleComplete, (log.id, ))
+        action = ds.query(Data.Model.Action).join(Data.Model.Trigger).filter(Data.Model.Trigger.id == trigger_id).first()
+        if action:
+            with self._handleLock:
+                if action.id in self._handles:
+                    self._handles[action.id].stop()
+                self._handles[action.id] = self._actionRunner.executeAsync(action, self._handleComplete, (log.id,))
         return log
