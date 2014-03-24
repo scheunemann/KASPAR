c31ec084868ced79e695ac8b1ccbcf3d6b9cc55b
Nathan Burke <n.burke@natbur.com>
2014-03-24 02:26:22 +0000

    update to use routes.js file

--- Web/gui/js/main.js
+++ Web/gui/js/main.js
@@ -48,7 +48,7 @@ require.config({
 // http://code.angularjs.org/1.2.1/docs/guide/bootstrap#overview_deferred-bootstrap
 window.name = "NG_DEFER_BOOTSTRAP!";
 
-require([ 'angular', 'app' ], function(angular, app) {
+require([ 'angular', 'app', 'routes' ], function(angular, app, routes) {
 	'use strict';
 	var $html = angular.element(document.getElementsByTagName('html')[0]);
 
