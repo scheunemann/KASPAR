2f79cf20c304006cc590fe55c78ba353a8154c59
Nathan Burke <n.burke@natbur.com>
2014-04-13 09:31:33 +0100

    update bower config, version bump

--- Web/gui/src/modules/common/CommonController.js
+++ Web/gui/src/modules/common/CommonController.js
@@ -4,7 +4,7 @@ define(function(require) {
 	var angular = require('angular');
 
 	var CommonController = function($scope, $rootScope) {
-		$scope.version = '3.0 Alpha 7';
+		$scope.version = '3.0 Alpha 10';
 
 //		$rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
 //			console.log('$stateChangeStart to ' + toState.to + '- fired when the transition begins. toState,toParams : \n', toState, toParams);
