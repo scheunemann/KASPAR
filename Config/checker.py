83c8223325e58d1ed9a053dd4cebaa3ee53a5f69
Nathan Burke <n.burke@natbur.com>
2014-06-23 18:55:33 +0100

    update checker/configurator

--- Config/checker.py
+++ Config/checker.py
@@ -10,3 +10,10 @@ mB = m.getConnection('/dev/bodySensors')
 
 ids = [1, 2, 3, 4, 5, 6, 16, 17, 18, 19, 20, 32, 33, 34, 35, 36]
 
+def loop(command, ids, *args):
+    output = []
+    for i in ids:
+        o = command(i, *args)
+        if o != None:
+            output.append(o)
+    return output
--- Config/servoConfigurator.py
+++ Config/servoConfigurator.py
@@ -18,7 +18,15 @@ from Robot import importer
 from Data import Model
 from Robot.ServoInterface.servoInterface import ServoInterface
 from Robot.ServoInterface.herkulex import HerkuleX
-herkulex = HerkuleX('COM25', 115200)
+if isWin:
+	herkulex = HerkuleX('COM25', 115200)
+else:
+	herkulex = HerkuleX('/dev/bodyServos', 115200)
+
+VOLTAGE_INPUT = 12
+VOLTAGE_VARIANCE = 2
+POSITION_VARIANCE = 10
+POSITION_PADDING = 20
 
 def chooseRobot(robots):
 	if len(robots) > 1:
@@ -53,7 +61,12 @@ def selectJoint(robot):
 	while selection == None:
 		print "Joints:"
 		for i in range(0, len(servos)):
-			print "%s - %s" % (str(i + 1).rjust(4), servos[i].jointName)
+			sid = servos[i].extraData.get('externalId', None)
+			if sid != None:
+				print "%s - %s (servoId: %s)" % (str(i + 1).rjust(4), servos[i].jointName, sid)
+			else:
+				print "%s - %s" % (str('N/A').rjust(4), servos[i].jointName)
+		print "%s - Re-configure all joints" % str(i + 2).rjust(4)
 		print "ctrl+c - Exit"
 		raw_sel = raw_input('Select Joint: ')
 		try:
@@ -63,14 +76,16 @@ def selectJoint(robot):
 			time.sleep(1)
 			clearScreen()
 		else:
-			if selection <= 0 or selection > len(servos) + 1:
+			if selection <= 0 or selection > len(servos) + 2:
 				print 'Invalid Selection'
 				time.sleep(1)
 				clearScreen()
 			else:
 				break
-	servo = servos[selection - 1]
-	return servo
+	if selection == len(servos) + 1:
+		return servos
+	else:
+		return servos[selection - 1]
 	
 def configureServo(servo):
 	newId = int(servo.extraData['externalId'])
@@ -93,28 +108,12 @@ def configureServo(servo):
 				break
 	else:
 		oldId = ids[0]
-	
 	while True:
 		try:
 			input = raw_input('Press Enter to configure servo %s or ctrl+c to change selection or rescan servos. ' % oldId)
-			autoloop = input == 'l'
 			clearScreen()
 			print "Configuring servoID %s as joint %s" % (oldId, servo.jointName)
-			herkulex.clearError(oldId)
-                        time.sleep(0.1)
-			setMaxVoltage(oldId, 14)
-			minPos = int(servoInt._scaleToRealPos(servoInt._minPos)) - 20
-			minPos = max(minPos, 0)
-			maxPos = int(servoInt._scaleToRealPos(servoInt._maxPos)) + 20
-			maxPos = min(maxPos, 1023)
-                        defPos = int(servoInt._scaleToRealPos(servoInt._defaultPos))
-                        defPos = min(maxPos, defPos)
-                        defPos = max(minPos, defPos)
-			setRange(oldId, minPos, maxPos)
-			reboot(oldId)
-			center(oldId, defPos)
-                	print "Stats - Position: %s, Voltage: %s, Errors: %s" % (herkulex.getPosition(oldId), herkulex.getVoltage(oldId), herkulex.error_text(oldId))
-			if isConfigured(oldId, defPos):
+			if doConfigure(oldId, servoInt):
 				setId(oldId, newId)
 				success(newId)
 				print "Success"
@@ -123,14 +122,50 @@ def configureServo(servo):
 		except KeyboardInterrupt:
 			break
 
+def reconfigure(servos):
+        for servo in servos:
+		sid = servo.extraData.get('externalId', None)
+		if not sid:
+			continue
+                sid = int(sid)
+		print "-----------------------------"
+		print "Re-configuring servo %s (%s)" % (servo.jointName, sid)
+		if not servo.model.name == "HERKULEX":
+			continue
+		servoInt = ServoInterface(servo)
+		if doConfigure(sid, servoInt):
+			success(sid)
+			print "Success"
+		else:
+			print "Servo not configured properly!"
+	print "-----------------------------"
+	print "            Done"
+        raw_input("   Press ENTER to contine")
+
+def doConfigure(servoId, servoInt):
+	herkulex.clearError(servoId)
+	time.sleep(0.1)
+	setMaxVoltage(servoId, 14)
+	minPos = int(servoInt._scaleToRealPos(servoInt._minPos)) - POSITION_PADDING
+	minPos = max(minPos, 0)
+	maxPos = int(servoInt._scaleToRealPos(servoInt._maxPos)) + POSITION_PADDING
+	maxPos = min(maxPos, 1023)
+        defPos = int(servoInt._scaleToRealPos(servoInt._defaultPos))
+        defPos = min(maxPos, defPos)
+        defPos = max(minPos, defPos)
+	setRange(servoId, minPos, maxPos)
+	reboot(servoId)
+	center(servoId, defPos)
+        print "Stats - Position: %s, Voltage: %s, Errors: %s" % (herkulex.getPosition(servoId), herkulex.getVoltage(servoId), herkulex.error_text(servoId))
+	return isConfigured(servoId, defPos)
 			
 def isConfigured(sid, defaultPos):
         pos = herkulex.getPosition(sid)
         time.sleep(0.1)
         volt = herkulex.getVoltage(sid)
         time.sleep(0.1)
-	posCorrect = abs(pos - defaultPos) < 5
-	voltCorrect = abs(volt - 12) < 0.5
+	posCorrect = abs(pos - defaultPos) < POSITION_VARIANCE
+	voltCorrect = abs(volt - VOLTAGE_INPUT) < VOLTAGE_VARIANCE
 	errors = herkulex.error_text(sid)
         time.sleep(0.1)
 	if errors:
@@ -138,7 +173,7 @@ def isConfigured(sid, defaultPos):
 	if not posCorrect:
                 print "Got unexpected position!  Got: %s, Expected: %s" % (pos, defaultPos)
         if not voltCorrect:
-                print "Got unexpected voltage!  Got: %s, Expected: %s" % (volt, 12)
+                print "Got unexpected voltage!  Got: %s, Expected: %s" % (volt, VOLTAGE_INPUT)
 	return posCorrect & voltCorrect & (not errors)
 
 def setId(id, newId):
@@ -187,7 +222,7 @@ def success(sid):
 	time.sleep(0.1)
 	herkulex.clearError(sid)
 	time.sleep(0.1)
-	
+
 if __name__ == '__main__':
 	clearScreen()
 	robot = chooseRobot(robots)
@@ -195,6 +230,9 @@ if __name__ == '__main__':
 		try:
 			clearScreen()
 			joint = selectJoint(robot)
-			configureServo(joint)
+			if isinstance(joint, list):
+				reconfigure(joint)
+			else:
+				configureServo(joint)
 		except KeyboardInterrupt:
 			break
