3ca6768d51d8a225a14808863efc92c6b5b99055
Nathan Burke <n.burke@natbur.com>
2014-08-13 13:58:10 +0100

    remove debug flags

--- Web/site.py
+++ Web/site.py
@@ -34,10 +34,10 @@ def configureSite():
     apiRoot.server = server
 
     # Disable connection for debugging without a robot
-    from Robot.ServoInterface import ServoInterface
-    from Processor.SensorInterface import SensorInterface
-    ServoInterface.disconnected = True
-    SensorInterface.disconnected = True
+    #from Robot.ServoInterface import ServoInterface
+    #from Processor.SensorInterface import SensorInterface
+    #ServoInterface.disconnected = True
+    #SensorInterface.disconnected = True
 
 
 def runSite():
