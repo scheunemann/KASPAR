8b8add86a9275c4a4cd325897f7c72a5efe9ec50
Nathan Burke <n.burke@natbur.com>
2014-08-23 21:36:33 +0100

    Remove old configs, update testData for import

--- testData.py
+++ testData.py
@@ -12,35 +12,12 @@ from Robot import importer
 
 def _flushAndFillTestData(configDir, flush=True):
     StorageFactory.config['engine'].update(dbConfig)
-    StorageFactory.config['debug'] = True
     if flush:
+        StorageFactory.getDefaultDataStore().engine.echo = True
         StorageFactory.drop_keys(StorageFactory.getDefaultDataStore().engine)
         Base.metadata.drop_all(StorageFactory.getDefaultDataStore().engine)
         Base.metadata.create_all(StorageFactory.getDefaultDataStore().engine)
-
-    operators = [
-                    Operator('Operator1', 'Test Operator 1'),
-                    Operator('Operator2', 'Test Operator 2'),
-                 ]
-
-    users = [
-             User('User1', 'Test User 1'),
-             User('User2', 'Test User 2'),
-             User('User3', 'Test User 3'),
-             User('User4', 'Test User 4'),
-             User('User5', 'Test User 5'),
-             User('User6', 'Test User 6'),
-             User('User7', 'Test User 7'),
-             User('User8', 'Test User 8'),
-             User('User9', 'Test User 9'),
-             User('User0', 'Test User 10'),
-             User('User11', 'Test User 11'),
-             User('User12', 'Test User 12'),
-             User('User13', 'Test User 13'),
-             User('User14', 'Test User 14'),
-             ]
-
-    operators[0].users.extend(users[1:3])
+        StorageFactory.getDefaultDataStore().engine.echo = False
 
     print "Loading configs..."
     (robots, actions, triggers) = importer.loadDirectory({}, {}, [], configDir)
@@ -48,20 +25,46 @@ def _flushAndFillTestData(configDir, flush=True):
 
     session = StorageFactory.getNewSession()
 
-    setting = Setting(key='robot', value=robots[0].name)
-    session.add(setting)
+    if flush:
 
-    interaction = Interaction()
-    interaction.startTime = datetime.datetime.utcnow()
-    interaction.endTime = datetime.datetime.utcnow()
-    interaction.operator = operators[0]
-    interaction.user = users[3]
-    session.add(interaction)
+        operators = [
+                        Operator('Operator1', 'Test Operator 1'),
+                        Operator('Operator2', 'Test Operator 2'),
+                     ]
+
+        users = [
+                 User('User1', 'Test User 1'),
+                 User('User2', 'Test User 2'),
+                 User('User3', 'Test User 3'),
+                 User('User4', 'Test User 4'),
+                 User('User5', 'Test User 5'),
+                 User('User6', 'Test User 6'),
+                 User('User7', 'Test User 7'),
+                 User('User8', 'Test User 8'),
+                 User('User9', 'Test User 9'),
+                 User('User0', 'Test User 10'),
+                 User('User11', 'Test User 11'),
+                 User('User12', 'Test User 12'),
+                 User('User13', 'Test User 13'),
+                 User('User14', 'Test User 14'),
+                 ]
+
+        operators[0].users.extend(users[1:3])
+        session.add_all(operators)
+
+        setting = Setting(key='robot', value=robots[0].name)
+        session.add(setting)
+
+        interaction = Interaction()
+        interaction.startTime = datetime.datetime.utcnow()
+        interaction.endTime = datetime.datetime.utcnow()
+        interaction.operator = operators[0]
+        interaction.user = users[3]
+        session.add(interaction)
 
     session.add_all([r for r in robots if r.name == setting.value])
     session.add_all(actions)
     session.add_all(triggers)
-    session.add_all(operators)
     session.add_all(users)
     session.commit()
     session.close()
@@ -71,13 +74,14 @@ if __name__ == "__main__":
         print "Subdir not specified"
 #         exit()
         subDir = 'kaspar-1c'
+        flush = True
     else:
         subDir = sys.argv[1]
 
-    if len(sys.argv) > 2:
-        flush = sys.argv[2].lower() == 'flush'
-    else:
-        flush = False
+        if len(sys.argv) > 2:
+            flush = sys.argv[2].lower() == 'flush'
+        else:
+            flush = False
 
     baseDir = os.path.dirname(os.path.realpath(__file__))
     configDir = os.path.join(baseDir, 'Config/kasparConfigs/' + subDir)
