9e7452fc363a3a57ed2dab6b7b073d9539717658
Nathan Burke <n.burke@natbur.com>
2014-08-26 14:16:31 +0000

    Fix saving poses

--- Web/gui/build/modules/operators/directives.js
+++
@@ -1,2 +0,0 @@
-define(["require","angular","common/i18n/languageServices"],function(require){{var angular=require("angular"),languageServices=require("common/i18n/languageServices"),moduleName="kasparGUI.operators.directives",dependancies=[languageServices];angular.module(moduleName,dependancies)}return moduleName});
-//# sourceMappingURL=directives.js.map
