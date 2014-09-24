6ff8a2f4b710eeb5277df110e0f81112daa54226
Nathan Burke <n.burke@natbur.com>
2014-09-24 23:14:32 +0100

    range updates for joints, updates from last meeting

--- kasparGUI/Web/gui/src/modules/actions/ActionController.js
+++ kasparGUI/Web/gui/src/modules/actions/ActionController.js
@@ -1,69 +1,89 @@
 'use strict';
 
 define(function(require) {
-	var angular = require('angular');
-	var _ = require('underscore');
-	require('actions/models');
-	require('actions/directives');
+        var angular = require('angular');
+        var _ = require('underscore');
+        require('actions/models');
+        require('actions/directives');
+        require('common/directives');
 
-	var ActionController = function($scope, $http, Action, ActionType, language) {
-		$scope.language = language.getText();
-		$scope.actions = Action.query();
-		$scope.types = ActionType.query();
+        var ActionController = function($scope, $http, Action, ActionType, language) {
+            $scope.typeFilter = {
+                type: '',
+            };
+            $scope.language = language.getText();
+            $scope.actions = Action.query();
+            $scope.type = null;
+            $scope.types = ActionType.query();
+            $scope.types.$promise.then(function(types) {
+                    var action = {
+                        name: 'Action'
+                    };
+                    $scope.types.push(action);
+                    $scope.type = action;
+                });
 
-		$scope.$watch('action', function(action) {
-			if (action !== undefined && action !== null) {
-				var abstractAction = $scope.action;
-				var concreteAction = $scope.action.getConcreteClassInstance();
-				concreteAction.$promise.then(function() {
-					$scope.actions[$scope.actions.indexOf(abstractAction)] = concreteAction;
-					$scope.action = concreteAction;
-				});
-			}
-			
-			if (action && !_.find($scope.actions, action)) {
-				$scope.actions.push(action);
-			}
-		});
+            $scope.$watch('type', function(type, old) {
+                    if (type && type.name != 'Action') {
+                        $scope.typeFilter.type = type.name;
+                    } else {
+                        $scope.typeFilter.type = '';
+                    }
+                });
 
-		$scope.newAction = function() {
-			$scope.action = new Action({
-				type : 'Action'
-			});
-			$scope.actions.push($scope.action);
-		};
+            $scope.$watch('action', function(action) {
+                    if (action !== undefined && action !== null) {
+                        var abstractAction = $scope.action;
+                        var concreteAction = $scope.action.getConcreteClassInstance();
+                        concreteAction.$promise.then(function() {
+                                $scope.actions[$scope.actions.indexOf(abstractAction)] = concreteAction;
+                                $scope.action = concreteAction;
+                            });
+                    }
 
-		$scope.deleteAction = function(action) {
-			$scope.actions.splice($scope.actions.indexOf(action), 1);
-			$scope.action = undefined;
-			if(action.$delete != undefined) {
-				action.$delete();
-			}
-		};
+                    if (action && !_.find($scope.actions, action)) {
+                        $scope.actions.push(action);
+                    }
+                });
 
-		var errorFunc = function(status) {
-			console.log("Error sending file:" + status);
-		};
+            $scope.newAction = function() {
+                $scope.action = new Action({
+                        type: $scope.type.name || 'Action',
+                    });
+                $scope.actions.push($scope.action);
+            };
 
-		var successFunc = function(data, status, headers, config) {
-			$scope.newobjs.push(data);
-		};
+            $scope.deleteAction = function(action) {
+                $scope.actions.splice($scope.actions.indexOf(action), 1);
+                $scope.action = undefined;
+                if (action.$delete != undefined) {
+                    action.$delete();
+                }
+            };
 
-		$scope.importFiles = function(files) {
+            var errorFunc = function(status) {
+                console.log("Error sending file:" + status);
+            };
 
-			for (var i = 0; i < files.length; i++) {
-				var fd = new FormData();
-				fd.append("data", files[i]);
+            var successFunc = function(data, status, headers, config) {
+                $scope.newobjs.push(data);
+            };
 
-				var obj = $http.post('/api/Action/Import', fd, {
-					headers : {
-						'Content-Type' : undefined
-					},
-					transformRequest : angular.identity
-				}).success(successFunc).error(errorFunc);
-			}
-		};
-	};
+            $scope.importFiles = function(files) {
 
-	return [ '$scope', '$http', 'Action', 'ActionType', 'language', ActionController ];
-});
+                for (var i = 0; i < files.length; i++) {
+                    var fd = new FormData();
+                    fd.append("data", files[i]);
+
+                    var obj = $http.post('/api/Action/Import', fd, {
+                            headers: {
+                                'Content-Type': undefined
+                            },
+                            transformRequest: angular.identity
+                        }).success(successFunc).error(errorFunc);
+                }
+            };
+        };
+
+        return ['$scope', '$http', 'Action', 'ActionType', 'language', ActionController];
+    });
---
+++ kasparGUI/Web/gui/src/modules/common/directives/Dropdown.js
@@ -0,0 +1,61 @@
+'use strict';
+
+define(function(require) {
+        var angular = require('angular');
+
+        var Dropdown = function($compile) {
+            return {
+                restrict: 'E',
+                scope: {
+                    items: '=ddOptions',
+                    selectedItem: '=ddModel'
+                },
+                link: function(scope, element, attrs) {
+                    var html = '';
+                    switch (attrs.menuType) {
+                        case "select":
+                            html += '<div class="btn-group"><button class="btn btn-default button-label dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button>';
+                            break;
+                        case "button":
+                            html += '<div class="btn-group"><button class="btn button-label btn-default"></button><button class="btn btn-default dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button>';
+                            break;
+                        default:
+                            html += '<div class="dropdown"><a class="dropdown-toggle" role="button" data-toggle="dropdown" href="javascript:;">Dropdown<b class="caret"></b></a>';
+                            break;
+                    }
+                    html += '<ul class="dropdown-menu"><li ng-repeat="item in items | orderBy:&apos;name&apos;"><a tabindex="-1" data-ng-click="selectVal(item)">{{item.name}}</a></li></ul></div>';
+                    element.append($compile(html)(scope));
+                    for (var i = 0; i < scope.items.length; i++) {
+                        if (scope.items[i].id === scope.selectedItem) {
+                            scope.bSelectedItem = scope.items[i];
+                            break;
+                        }
+                    }
+                    scope.selectVal = function(item) {
+                        var label = '';
+                        if (item) {
+                            label = item.name;
+                        } else {
+                            label = attrs.prompt;
+                        }
+                        switch (attrs.menuType) {
+                            case "select":
+                                $('button.button-label', element).html(label + ' <span class="caret"></span>');
+                                break;
+                            case "button":
+                                $('button.button-label', element).html(label);
+                                break;
+                            default:
+                                $('a.dropdown-toggle', element).html('<b class="caret"></b> ' + label);
+                                break;
+                        }
+                        scope.selectedItem = item;
+                    };
+                    scope.selectVal(scope.bSelectedItem);
+                }
+            };
+        };
+
+        return ['$compile', Dropdown];
+
+    });
