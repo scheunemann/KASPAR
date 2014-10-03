6a228d9f80601046f1ff31acb7c364f9c3005e0e
Nathan Burke <n.burke@natbur.com>
2014-10-03 20:47:49 +0100

    Move interaction and interaction log models to teacher module

---
+++ kasparGUI/Web/gui/src/modules/teacher/models/InteractionLog.js
@@ -0,0 +1,16 @@
+'use strict';
+
+define(function(require) {
+        var Interaction = function(modelBuilder) {
+            var _service = modelBuilder.getModel('InteractionLog', {}, {
+                    get: {
+                        method: 'GET',
+                        cache: false
+                    }
+                }, 'Interaction/:interactionId/Log/:id');
+
+            return _service;
+        };
+
+        return ['modelBuilder', Interaction];
+    });
