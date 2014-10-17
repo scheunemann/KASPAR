94ef1f129ccb5aab7aa1ca63c190c107854142b9
Nathan Burke <n.burke@natbur.com>
2014-10-17 17:49:14 +0000

    Update config files for multiple kaspars

--- systemConfigs/configure.sh
+++ systemConfigs/configure.sh
@@ -1,35 +1,111 @@
 #!/bin/bash
 
-if [ "$(whoami)" != "root" ]
-  then
-    echo "Script must be run with sudo"
+function usage() {
+    echo "Usage: $0 kasparNumber"
     exit 1
-fi
+}
 
+function checkRoot() {
+    if [ "$(whoami)" != "root" ]; then
+        echo "Script must be run with sudo"
+        exit 1
+    fi
+    return 0
+}
 
-echo "Configuring WiFi Access Point HOST"
-update-rc.d hostapd enable
-update-rc.d udhcpd enable
+function checkInput() {
+    local __number=''
+    re='^[0-9]+$'
+    if [[ $1 =~ $re ]]; then
+        __number=$1
+    fi
+    if [ -z "$__number" ]; then
+        read -p "Enter KASAPR Number (1-20): " __number
+    fi
+    echo $__number
+    return 0
+}
 
-echo ""
-echo "Configuring WebUI Service"
-update-rc.d kasparweb enable
+function printHeader() {
+    echo "-------------------------"
+    echo "  Configuring $1"
+    echo "-------------------------"
+    return 0
+}
 
-echo ""
-echo "Installing bluetooth software"
-apt-get install bluetooth bluez-utils -yq
+function setName() {
+    echo ""
+    echo "Setting hostname to: $1"
+    hostname $1
+    echo $1 > /etc/hostname
+    __hostString="127.0.0.1 $1"
+    if ! grep -Fxq "$__hostString" /etc/hosts; then
+        echo $__hostString >> /etc/hosts
+    fi
+    return 0
+}
 
-echo ""
-echo "Installing i2c software"
-apt-get install python-smbus -yq
-apt-get install i2c-tools -yq
+function configureWifi() {
+    __ssid=`echo $1 | awk '{print tolower($0)}'`
+    echo ""
+    echo "Configuring WiFi Access Point HOST SSID: $__ssid"
+    updateWifiSSID $__ssid
+    update-rc.d hostapd enable
+    update-rc.d udhcpd enable
+    return 0
+}
 
-echo ""
-echo "Installing audio bindings"
-apt-get install portaudio19-dev -yq
+function updateWifiSSID() {
+    sed -i "s/ssid=.*/ssid=$1/g" /etc/hostapd/hostapd.conf
+    return 0
+}
 
-echo ""
-echo "Don't forget to run confgureRTC after rebooting"
-read -p "Press [Enter] to reboot NOW, or CTRL+C to exit"
+function configureWeb() {
+    echo ""
+    echo "Configuring WebUI Service"
+    update-rc.d kasparweb enable
+    return 0
+}
 
-echo reboot
+function configureBluetooth() {
+    echo ""
+    echo "Installing bluetooth software"
+    apt-get install bluetooth bluez-utils -yq
+    return 0
+}
+
+function configureRTC() {
+    echo ""
+    echo "Installing i2c software"
+    apt-get install python-smbus -yq
+    apt-get install i2c-tools -yq
+    return 0
+}
+
+function configureAudio() {
+    echo ""
+    echo "Installing audio bindings"
+    apt-get install portaudio19-dev -yq
+    return 0
+}
+
+function reboot() {
+    echo ""
+    echo "Don't forget to run confgureRTC after rebooting"
+    read -p "Press [Enter] to reboot NOW, or CTRL+C to exit"
+    reboot
+    return 0
+}
+
+checkRoot
+number=`checkInput $1`
+name=KASPAR3-$number
+
+printHeader $name
+setName $name
+configureWifi $name
+configureWeb
+configureBluetooth
+configureRTC
+configureAudio
+reboot
