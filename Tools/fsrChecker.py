5c914c5c55c82be489e2ae2b1adf54d04503347c
Nathan Burke <n.burke@natbur.com>
2014-11-10 16:25:12 +0000

    readablity cleanup

--- Tools/fsrChecker.py
+++ Tools/fsrChecker.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import time
+import itertools
 
 ids = {
        '/dev/bodySensors': [0, 1, 2, 3, 4, 5, 6, 8, 9, 10, 11],
@@ -17,17 +18,19 @@ def getConnection(port='/dev/headServos'):
 
 def doScan(ids):
     conns = dict(map(lambda x: (x, getConnection(x)), ids.keys()))
+    length = max([len(x) for x in ids.keys()]) + 1
     while True:
         try:
             vals = {}
             for (port, conn) in conns.iteritems():
                 if port not in ids:
                     continue
-                prefix = port.replace('/dev/', '')[0]
+                #prefix = port.replace('/dev/', '')[0]
                 for k in ids[port]:
-                    vals["%s%02d" % (prefix, k)] = conn.getPosition(k)
-                
-            print ["%s: %03d" % (k, vals[k]) for k in sorted(vals.keys())]
+                    vals["%s/%02d" % (port, k)] = conn.getPosition(k)
+            
+            for (prefix, items) in itertools.groupby(sorted(vals.keys()), lambda x: x[:x.rindex('/')]):
+                print "%s: %s" % (prefix.rjust(length), ["%s: %03d" % (k[k.rindex('/')+1:], vals[k]) for k in sorted(items)])
             time.sleep(0.25)
         except KeyboardInterrupt:
             break
